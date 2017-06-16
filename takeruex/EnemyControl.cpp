9cf5babbddecf497ea46e108df2f42e3de3d692b
永海　拓真 <1031takuma@gmail.com>
2017-06-16 17:15:36 +0900

    エネミー追尾移動（コメント化してます）

--- takeruex/EnemyControl.cpp
+++ takeruex/EnemyControl.cpp
@@ -1,1 +1,44 @@
 #include"EnemyControl.h"
+#include"MapRender.h"
+#include"MapControl.h"
+#include"CommonRender.h"
+#include"CommonControl.h"
+#include"MainControl.h"
+#include"PlayerRender.h"
+#include"PlayerControl.h"
+
+//
+//Enemy g_enemy;
+//
+//
+//void EnemyInit() {
+//	g_enemy.WindowPos.x = g_enemy.WorldPos.x = 222;
+//	g_enemy.WindowPos.y = g_enemy.WorldPos.y = 202;
+//	g_enemy.dmg = 20;
+//	g_enemy.bedead=false;
+//	g_enemy.beActive=false;
+//	g_enemy.beLeft=false;
+//
+//}
+////Player* GetplayerData()
+////D3DXVECTOR2* GetBasePoint()
+//void EnemyMove() {
+//	Player* player = GetplayerData();
+//
+//
+//	//エネミーのX座標がプレイヤーのX座標より小さかったら
+//	if (player->WindowPos.x < g_enemy.WindowPos.x) {
+//		//+方向にエネミーを動かす
+//		g_enemy.WindowPos.x += MOVE_SUPEED;
+//	}
+//	//エネミーのX座標がプレイヤーのX座標より大きかったら
+//	else if (player->WindowPos.x > g_enemy.WindowPos.x) {
+//		//-方向にエネミーを動かす
+//		g_enemy.WindowPos.x -= MOVE_SUPEED;
+//	}
+//}
+//
+//	Enemy* GetenemyData() {
+//		return &g_enemy;
+//	}
+//
--- takeruex/EnemyControl.h
+++ takeruex/EnemyControl.h
@@ -2,15 +2,23 @@
 #define ENEMYCONTROL_H
 
 #include<d3dx9.h>
+#define MOVE_SUPEED 3.0f
 
-struct Enemy {
-	D3DXVECTOR2 WindowPos;
-	D3DXVECTOR2 WorldPos;
-	int dmg;
-	bool bedead;
-	bool beActive;
-	bool beLeft;
-};
+
+
+//struct Enemy { 
+//	D3DXVECTOR2 WindowPos;
+//	D3DXVECTOR2 WorldPos;
+//	int dmg;
+//	bool bedead;//死んでいるか
+//	bool beActive;//活動中か
+//	bool beLeft;//左（右）どうっち向いてるか
+//};
+//
+//
+////エネミーデータ
+//Player* GetenemyData();
+//
 
 
 #endif
