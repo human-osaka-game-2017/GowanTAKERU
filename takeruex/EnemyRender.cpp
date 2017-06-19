3d0f9fff3103b57d5daed60205fd3a716d581edb
佐伯 <rateria396@gmail.com>
2017-06-19 11:41:22 +0900

    敵の描画

--- takeruex/EnemyRender.cpp
+++ takeruex/EnemyRender.cpp
@@ -13,24 +13,27 @@ void EnemyRender() {
 	Enemy* enemy = GetenemyData();
 
 	CUSTOMVERTEX Enemy[] = {
-		{ -ENEMYRESIZEWIDHE / 2,-ENEMYRESIZEHEIGHT / 2,0.5f,1.0f, 0xFFFFFFFF,0.0f,0.0f },
-		{ ENEMYRESIZEWIDHE / 2,-ENEMYRESIZEHEIGHT / 2,0.5f,1.0f,0xFFFFFFFF,1.0f,0.0f },
-		{ ENEMYRESIZEWIDHE / 2,ENEMYRESIZEHEIGHT / 2,0.5f,1.0f,0xFFFFFFFF,1.0f,1.0f },
-		{ -ENEMYRESIZEWIDHE / 2,ENEMYRESIZEHEIGHT / 2,0.5f,1.0f,0xFFFFFFFF,0.0f,1.0f }
+		{-ENEMYRESIZEWIDHE / 2, -ENEMYRESIZEHEIGHT / 2, 0.5f,1.0f,0xFFFFFFFF,0.0f,0.0f },
+		{ ENEMYRESIZEWIDHE / 2, -ENEMYRESIZEHEIGHT / 2, 0.5f,1.0f,0xFFFFFFFF,1.0f,0.0f },
+		{ ENEMYRESIZEWIDHE / 2,  ENEMYRESIZEHEIGHT / 2, 0.5f,1.0f,0xFFFFFFFF,1.0f,1.0f },
+		{-ENEMYRESIZEWIDHE / 2,  ENEMYRESIZEHEIGHT / 2, 0.5f,1.0f,0xFFFFFFFF,0.0f,1.0f }
 	};
 
-	
+	if (enemy->beDead == false && enemy->beActive == true)
+	{
 
-	CUSTOMVERTEX DrawVertex[4];
-	for (int i = 0; i < 4; i++) {
-		DrawVertex[i] = Enemy[i];
-		DrawVertex[i].x += enemy->WindowPos.x;
-		DrawVertex[i].y += enemy->WindowPos.y;
+		CUSTOMVERTEX DrawVertex[4];
+		for (int i = 0; i < 4; i++) {
+			DrawVertex[i] = Enemy[i];
+			DrawVertex[i].x += enemy->WindowPos.x;
+			DrawVertex[i].y += enemy->WindowPos.y;
+		}
+
+		// テクスチャをステージに割り当てる
+		pD3Device->SetTexture(0, pTexture[ENEMY01_TEX]);
+		// 描画
+		pD3Device->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, DrawVertex, sizeof(CUSTOMVERTEX));
 	}
 
-	// テクスチャをステージに割り当てる
-	pD3Device->SetTexture(0, pTexture[ENEMY01_TEX]);
-	// 描画
-	pD3Device->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, DrawVertex, sizeof(CUSTOMVERTEX));
 
 }
