4706fa2682a2ac61c6d1cf856a70862225143a47
永海　拓真 <1031takuma@gmail.com>
2017-06-22 12:14:16 +0900

    Revert "エネミーをプレイヤーの方に動くようにと活動中フラグ建てました"
    This reverts commit 612c0e0f0d1e0e0ee8f8c44e932938773356d5d6.

--- takeruex/EnemyControl.cpp
+++ takeruex/EnemyControl.cpp
@@ -15,38 +15,26 @@ Enemy* GetenemyData() {
 
 void EnemyInit() {
 
-
 }
 
-void EnemyMove(int enemyNum) {
-	Player* player = GetplayerData();
+////Player* GetplayerData()
+////D3DXVECTOR2* GetBasePoint()
+//void EnemyMove() {
+//	Player* player = GetplayerData();
+//
+//
+//	//エネミーのX座標がプレイヤーのX座標より小さかったら
+//	if (player->WindowPos.x < g_enemy.WindowPos.x) {
+//		//+方向にエネミーを動かす
+//		g_enemy.WindowPos.x += MOVE_SUPEED;
+//	}
+//	//エネミーのX座標がプレイヤーのX座標より大きかったら
+//	else if (player->WindowPos.x > g_enemy.WindowPos.x) {
+//		//-方向にエネミーを動かす
+//		g_enemy.WindowPos.x -= MOVE_SUPEED;
+//	}
+//}
+//
+void EnemyControl() {
 
-	//エネミーのX座標がプレイヤーのX座標より小さかったら
-	if (player->WindowPos.x < g_enemy[enemyNum].WindowPos.x) {
-		//+方向にエネミーを動かす
-		g_enemy[enemyNum].WindowPos.x += MOVE_SUPEED;
-	}
-	//エネミーのX座標がプレイヤーのX座標より大きかったら
-	else if (player->WindowPos.x > g_enemy[enemyNum].WindowPos.x) {
-		//-方向にエネミーを動かす
-		g_enemy[enemyNum].WindowPos.x -= MOVE_SUPEED;
-	}
-}
-void EnemyControl() {  
-		
-	D3DXVECTOR2* basepoint = GetBasePoint();
-	for (int i = 0; i < ENEMYNUMBER ; i++) {
-		//ウィンドウの外（左右）120ピクセルから描画開始のフラグを立てる
-		if (g_enemy[i].WorldPos.x < basepoint->x + DISPLAY_WIDTH / 2 + 120 || g_enemy[i].WorldPos.x > basepoint->x - DISPLAY_WIDTH / 2 + 120) {
-			g_enemy[i].beActive = true;
-		}
-
-		//エネミーとベースポイントとのworld,X座標の距離を調べる
-		float EnemyWorldDistance = g_enemy[i].WorldPos.x - basepoint->x;
-		//エネミーのwindow,X座標を調べる
-		g_enemy[i].WindowPos.x = basepoint->x + DISPLAY_WIDTH / 2 + EnemyWorldDistance;
-
-
-
-	}
 }
